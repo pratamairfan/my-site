8aab6aeb085ae780a88ff30310116aa73215b096
pratamairfan <pratamairfan58@gmail.com>
2025-11-17 12:23:59 +0700

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    "no-unused-vars": "off",
     ignores: [
       "node_modules/**",
       ".next/**",
---
+++ lib/auth.ts
@@ -0,0 +1,33 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "./generated/prisma/client";
+import { nextCookies } from "better-auth/next-js";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, verificationUrl }) => {
+      // Implement your email sending logic here
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email",
+        text: `Click the link to verify your email: ${verificationUrl}`,
+      });
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
